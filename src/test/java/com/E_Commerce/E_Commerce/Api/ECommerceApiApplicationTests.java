ec609fa9570dbbb082de952e2c7936837c2b12ac
Jose Alberto Rodriguez Cruz <45206681+jose2806@users.noreply.github.com>
2025-03-03 16:21:55 -0600

    update files

---
+++ src/test/java/com/E_Commerce/E_Commerce/Api/ECommerceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.E_Commerce.E_Commerce.Api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

27b91eaada5c79d661bc0747a8d6b1ab536fceb7
jose2806 <jose12.jr610@gmail.com>
2025-03-07 09:52:26 -0600

    Add Status

---
+++ src/main/java/com/E_Commerce/E_Commerce/Api/domain/order/Status.java
@@ -0,0 +1,7 @@
+package com.E_Commerce.E_Commerce.Api.domain.order;
+
+public enum Status {
+    PENDING,
+    PAID,
+    CANCELLED
+}
---
+++ src/main/java/com/E_Commerce/E_Commerce/Api/domain/payment/Status.java
@@ -0,0 +1,6 @@
+package com.E_Commerce.E_Commerce.Api.domain.payment;
+
+public enum Status {
+    SUCCESS,
+    FAILED
+}
